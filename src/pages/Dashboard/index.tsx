29d8a368d246685216b493c8602cf837ac318130
Wesley Siqueira <igorusp@gmail.com>
2022-03-18 22:55:34 -0300

    feat: make input functional

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,33 +1,73 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 
-import { Title, Form, Repos } from './styles';
+import { api } from '../../services/api';
+import { Title, Form, Repos, Error } from './styles';
 import { FiChevronRight } from 'react-icons/fi';
 
 import logo from '../../assets/logo.svg';
 
+interface GithubRepository {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
+
 export const Dashboard: React.FC = () => {
+  const [repos, setRepos] = useState<GithubRepository[]>([]);
+  const [newRepo, setNewRepo] = useState('');
+  const [inputError, setInputError] = useState('');
+
+  const handleInputChange = useCallback(
+    (event: React.ChangeEvent<HTMLInputElement>) =>
+      setNewRepo(event.target.value),
+    [],
+  );
+
+  async function handleAddRepo(
+    event: React.FormEvent<HTMLFormElement>,
+  ): Promise<void> {
+    event.preventDefault();
+    if (!inputError) {
+      setInputError('Informe o username/repositório');
+      return;
+    }
+    const response = await api.get<GithubRepository>(`repos/${newRepo}`);
+
+    const repository = response.data;
+
+    setRepos([...repos, repository]);
+    setNewRepo('');
+  }
   return (
     <>
       <img src={logo} alt="GitCollection" />
       <Title>Catálogos de repositórios do GitHub</Title>
 
-      <Form>
-        <input placeholder="username/repository_name" />
+      <Form hasError={Boolean(inputError)} onSubmit={handleAddRepo}>
+        <input
+          placeholder="username/repository_name"
+          onChange={handleInputChange}
+        />
         <button type="submit">Buscar</button>
       </Form>
-
+      {inputError && <Error>{inputError}</Error>}
       <Repos>
-        <a href="/repositories">
-          <img
-            src="https://avatars.githubusercontent.com/u/15800848?v=4"
-            alt="Repositorio"
-          />
-          <div>
-            <strong>wesleysiqueira/curso</strong>
-            <p>Repositorio do Wesley</p>
-          </div>
-          <FiChevronRight />
-        </a>
+        {repos.map(repository => (
+          <a href="/repositories" key={repository.full_name}>
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
+            <FiChevronRight />
+          </a>
+        ))}
       </Repos>
     </>
   );
